181c29731fd7057783a3820848d681c632fa46e9
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-01-27 18:29:00 +1000

    Updating meme text functionality added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,38 +4,69 @@ import { useState } from "react";
 const Meme = () => {
 
     const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
+        topText: "Shut up",
+        bottomText: "and take my money",
         randomImageUrl: "https://i.imgflip.com/3si4.jpg"
     })
 
     const [allMemes, setAllMemes] = useState(memesData);
 
-    function getNewMeme(){
+    function getNewImage(){
         const memes = allMemes.data.memes;
         const randomMeme = memes[Math.floor(Math.random()*memes.length)];
-        setMeme((prevMeme) => {
+        setMeme(prevMeme => {
             return {
                 ...prevMeme,
                 randomImageUrl: randomMeme.url
             }
         })
     }
+
+    function updateMeme(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     return ( 
        <main>
             <div className="form">
                 <div className="form-input-container">
                     <label className="form-label">Top text</label>
-                    <input type="text" className="form-input" placeholder="Shut up"/>
+                    <input 
+                        type="text" 
+                        className="form-input" 
+                        placeholder="Shut up"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={updateMeme}
+                    />
                 </div>
                 <div className="form-input-container">
                     <label className="form-label">Bottom text</label>
-                    <input type="text" className="form-input" placeholder="and take my money"/>
+                    <input 
+                        type="text" 
+                        className="form-input" 
+                        placeholder="and take my money"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={updateMeme}
+                    />
                 </div>
             
-                <button className="form-button" onClick={getNewMeme}>Get a new meme image  🖼</button>
+                <button className="form-button" onClick={getNewImage}>Get a new meme image  🖼</button>
             </div>
-            <img src={meme.randomImageUrl} alt="Meme image" className="meme-image" />
+
+            <div className="meme">
+                <img src={meme.randomImageUrl} alt="Meme image" className="meme-image" />
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
+            
        </main>
      );
 }
